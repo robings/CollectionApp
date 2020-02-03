77aa254ac2146742ec46e1315cde6e58e8b8fe96
robings <r_gordon_stables@hotmail.com>
2020-02-03 15:17:27 +0000

    added collection display

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once ('functions.php');
 
 
 
@@ -14,9 +15,22 @@
 
 <body>
 <section>
-    <div>
+    <div class='heroImage'>
         <h1>Shinkansen - Japanese Bullet Train Collection</h1>
     </div>
+</section>
+<section class='collection'>
+    <article>
+        <div class='item'>
+            <h2>N700</h2>
+            <img src='images/bullet-train-1918480_1280.jpg' alt='N700 Bullet Train'/>
+            <ul>
+                <li><span>Introduced:</span> 2007</li>
+                <li><span>Top speed:</span> 300km/h (185 mph)</li>
+                <li><span>Withdrawn:</span> still in service</li>
+            </ul>
+        </div>
+    </article>
 </section>
 
 </body>

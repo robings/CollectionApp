94b8a30d20f11f60e72becf877b798189dd34d65
robings <r_gordon_stables@hotmail.com>
2020-02-03 14:48:45 +0000

    added banner image and title

--- index.php
+++ index.php
@@ -8,9 +8,16 @@
 <html>
 <head>
    <title>Shinkansen</title>
+    <link rel='stylesheet' type='text/css' href='normalize.css' />
+    <link rel='stylesheet' type='text/css' href='styles.css' />
 </head>
 
 <body>
+<section>
+    <div>
+        <h1>Shinkansen - Japanese Bullet Train Collection</h1>
+    </div>
+</section>
 
 </body>
 

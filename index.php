4299522444e3eb2dd0f924aa791f771c6e957418
robings <r_gordon_stables@hotmail.com>
2020-02-03 15:48:35 +0000

    responsiveness including testing

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once ('functions.php');
 <html>
 <head>
    <title>Shinkansen</title>
+    <meta name="viewport" content="width=device-width" />
     <link rel='stylesheet' type='text/css' href='normalize.css' />
     <link rel='stylesheet' type='text/css' href='styles.css' />
 </head>
@@ -20,6 +21,28 @@ require_once ('functions.php');
     </div>
 </section>
 <section class='collection'>
+    <article>
+        <div class='item'>
+            <h2>N700</h2>
+            <img src='images/bullet-train-1918480_1280.jpg' alt='N700 Bullet Train'/>
+            <ul>
+                <li><span>Introduced:</span> 2007</li>
+                <li><span>Top speed:</span> 300km/h (185 mph)</li>
+                <li><span>Withdrawn:</span> still in service</li>
+            </ul>
+        </div>
+    </article>
+    <article>
+        <div class='item'>
+            <h2>N700</h2>
+            <img src='images/bullet-train-1918480_1280.jpg' alt='N700 Bullet Train'/>
+            <ul>
+                <li><span>Introduced:</span> 2007</li>
+                <li><span>Top speed:</span> 300km/h (185 mph)</li>
+                <li><span>Withdrawn:</span> still in service</li>
+            </ul>
+        </div>
+    </article>
     <article>
         <div class='item'>
             <h2>N700</h2>

ac8b5d8da84ec706428a360563ba83feeffa992f
robings <r_gordon_stables@hotmail.com>
2020-02-04 13:29:37 +0000

    failure tests added for collectionBox function - need to fix bugs

--- tests/functions.php
+++ tests/functions.php
@@ -13,6 +13,13 @@ class FunctionTests extends TestCase
         $case= collectionBox($input);
         $this->assertEquals($expected, $case);
     }
+    public function testCollectionBoxFailure() {
+        $expected = '<article><div class=\'item\'><h2>Series 0</h2><img src=\'images/japan-974730_1920.jpg\' alt=\'Series 0 Bullet Train\' /><ul><li><span>Introduced:</span> 1964</li><li><span>Top speed:</span> 220km/h (135mph)</li><li><span>Withdrawn:</span> 2008</li></ul></div></article>';
+        $input = [ 'id' =>'1', 'version'=>'0', 'introducedYR'=>'1964', 'topSpeedKMPH'=>'220', 'topSpeedMPH'=>'135', 'withdrawn'=>'1', 'withdrawnYR'=>'2008', 'imgURL'=>'images/japan-974730_1920.jpg'];
+
+        $case= collectionBox($input);
+        $this->assertEquals($expected, $case);
+    }
     public function testCollectionBoxMalformed() {
         $this->expectException(TypeError::class);
         $input = 1;

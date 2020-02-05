da0f3354fef02471ccd263e284aa3561564727f5
robings <r_gordon_stables@hotmail.com>
2020-02-05 13:44:57 +0000

    added form and html side validation

--- addForm.php
+++ addForm.php
@@ -25,8 +25,30 @@ $db = connectdb();
     </div>
 </header>
 <section>
-    <form>
-
+    <form action='addForm.php' method='post'>
+        <h2>Add Shinkansen</h2>
+        <div>
+        <label>Top speed (km/h) </label>
+        <input type='text' name='topSpeedKmh' size='5' maxlength='3' pattern='^\d{1,3}$' required />
+        </div>
+        <div>
+        <label>Top speed (mph) </label>
+        <input type='text' name='topSpeedMph' size='5' maxlength='3' pattern='^\d{1,3}$' required />
+        </div>
+        <div>
+        <label>Year introduced </label>
+        <input type='text' name='introYr' size='5' maxlength='4' pattern='^\d{1,4}$' required />
+        </div>
+        <div>
+        <label>Year withdrawn </label>
+        <input type='text' name='withdrawnYr' size='5' maxlength='4' pattern='^\d{1,4}$' />
+        <label>(leave blank if still in service)</label>
+        </div>
+        <div>
+        <label>Img url </label>
+        <input type='text' name='imgUrl' size='30' maxlength='500' required />
+        </div>
+        <input type='submit' value='Add' />
     </form>
 </section>
 

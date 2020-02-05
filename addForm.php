2e1e58afa7eddfbe724e5929251994149777ce9a
robings <r_gordon_stables@hotmail.com>
2020-02-05 11:42:06 +0000

    added new page, header and links between the two pages (no styling yet)

--- addForm.php
+++ addForm.php
@@ -5,17 +5,12 @@ require_once 'dbConnect.php';
 
 $db = connectdb();
 
-$shinkansens = getAllTrains($db);
-
-$trains = displayTrains($shinkansens);
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang='en-GB'>
 <head>
-   <title>Shinkansen</title>
+    <title>Shinkansen - Add Train</title>
     <meta name='viewport' content='width=device-width' />
     <link href='https://fonts.googleapis.com/css?family=Roboto&display=swap' rel='stylesheet'>
     <link rel='stylesheet' type='text/css' href='normalize.css' />
@@ -26,13 +21,14 @@ $trains = displayTrains($shinkansens);
 <header>
     <div class='heroImage'>
         <h1>Shinkansen - Japanese Bullet Train Collection</h1>
-        <div class='linkMover'><a href='addForm.php'>Add</a></div>
+        <div class='linkMover'><a href='index.php'>Home</a></div>
     </div>
 </header>
-<section class='collection'>
-    <?php echo $trains; ?>
+<section>
+    <form>
+
+    </form>
 </section>
 
 </body>
-
 </html>
